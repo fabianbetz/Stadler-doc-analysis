7371655e9bf3e09e655ee1c5a542a9fb4dc412ca
fabianbetz <fabian@betzing.de>
2024-11-21 15:01:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ api_key = st.secrets["OPENAI_API_KEY"]
 client = OpenAI(api_key=api_key)
 
 # Feste Assistant-ID und Nachricht
-ASSISTANT_ID = "asst_vvaFZVcZ4wbm3yetLeB3CTgj"
+ASSISTANT_ID = "asst_QTWXEpSDLdOarigrlq1Rpg4g"
 DEFAULT_MESSAGE = "Analyze the PDF following your instructions. Analyze the whole document. Execute your whole task."
 
 # Funktionen
